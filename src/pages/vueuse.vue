2f046c9451938a1af8bd7f6b46b915ebee83c265
李贤斌 <xianbin.me@qq.com>
2023-07-27 10:23:43 +0800

    add:添加vueuse的示例页面

---
+++ src/pages/vueuse.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+defineOptions({
+  name: 'IndexPage',
+})
+const activeNames = reactive({ // 卡片展开
+  one: 1,
+})
+const { x, y } = useMouse()
+
+// const user = useUserStore()
+// const name = ref(user.savedName)
+
+// const router = useRouter()
+// function go() {
+//   if (name.value)
+//     router.push(`/hi/${encodeURIComponent(name.value)}`)
+// }
+
+// const { t } = useI18n()
+//
+</script>
+
+<template>
+  <div id="vueuse" p-3>
+    <el-card class="box-card" w-400px>
+      <template #header>
+        <div class="card-header">
+          <span>useMouse</span>
+        </div>
+      </template>
+      <div class="text item">
+        {{ x }},{{ y }}
+      </div>
+      <el-collapse v-model="activeNames.one">
+        <el-collapse-item title="Consistency" name="1">
+          <div>
+            <pre>
+              <code>
+console.log(111)
+              </code>
+          </pre>
+          </div>
+        </el-collapse-item>
+      </el-collapse>
+    </el-card>
+  </div>
+</template>
+
+<route lang="yaml">
+meta:
+  layout: home
+</route>
